0ec0ba8be5e6a1e8b0b9342bf07b5f12b8744e10
Alfonso <alfonso@mbp.local>
2024-02-14 12:08:18 -0500

    add filters to add req/res correlation id headers

---
+++ src/main/java/com/socarras/gatewayserver/filter/ResponseTraceFilter.java
@@ -0,0 +1,34 @@
+package com.socarras.gatewayserver.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+@Configuration
+public class ResponseTraceFilter {
+
+    private final FilterUtility filterUtility;
+
+    public ResponseTraceFilter(FilterUtility filterUtility) {
+        this.filterUtility = filterUtility;
+    }
+
+    @Bean
+    public GlobalFilter postGlobalFilter() {
+        return (exchange, chain) ->
+                chain.filter(exchange).then(Mono.fromRunnable(() -> {
+                    HttpHeaders requestHeaders = exchange.getRequest().getHeaders();
+                    String correlationId = filterUtility.getCorrelationId(requestHeaders);
+
+                    exchange.getResponse()
+                            .getHeaders()
+                            .add(FilterUtility.CORRELATION_ID, correlationId);
+
+                    log.debug("Updated the correlation id to the outbound headers: {}", correlationId);
+                }));
+    }
+}
